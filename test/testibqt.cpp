b38f67af1b2cbd54c65ab6eb7f84464f7b8eea21
4FF <q139@live.com>
2020-10-20 20:14:15 +0300

    test3
    add test for lmt ord+bracket order

--- test/testibqt.cpp
+++ test/testibqt.cpp
@@ -11,12 +11,12 @@ TestIbQt::TestIbQt(QObject *parent) : QObject(parent)
 
 }
 
-void TestIbQt::marketOrder(char dir, QByteArray stockName, int quantity)
+void TestIbQt::marketOrder(char dir, QByteArray stockName,QByteArray secType, int quantity)
 {
-    qDebug() << "orderTest";
+
     Contract c;
     c.symbol = stockName;
-    c.secType = QByteArray("STK");
+    c.secType = secType;
     c.exchange = QByteArray("SMART");
     c.currency = QByteArray("USD");
 
@@ -29,9 +29,94 @@ void TestIbQt::marketOrder(char dir, QByteArray stockName, int quantity)
 
       order.orderType = "MKT";
       order.totalQuantity = quantity;
+       order.transmit=1;
 
     ib.placeOrder(ib.getOrderId(),c,order);
 
+     qDebug() << "marketOrder";
+
+}
+
+void TestIbQt::limitOrder(char dir, QByteArray stockName, QByteArray secType, int quantity, double price)
+{
+    Contract c;
+    c.symbol = stockName;
+    c.secType = secType;
+    c.exchange = QByteArray("SMART");
+    c.currency = QByteArray("USD");
+
+    Order order;
+    if(dir==1){
+        order.action = "BUY";
+    }else if(dir==-1){
+        order.action = "SELL";
+    }
+      order.lmtPrice = price;
+      order.orderType = "LMT";
+      order.totalQuantity = quantity;
+      order.transmit=1;
+
+    ib.placeOrder(ib.getOrderId(),c,order);
+
+     qDebug() << "marketOrder";
+
+
+}
+
+void TestIbQt::limitOrder(char dir, QByteArray stockName, QByteArray secType, int quantity, double price, double sl, double tp)
+{
+     Order parent;
+     Order takeProfit;
+     Order stopLoss;
+
+     if(dir==1){
+         parent.action = "BUY";
+     }else if(dir==-1){
+         parent.action = "SELL";
+     }
+
+     Contract c;
+     c.symbol = stockName;
+     c.secType = secType;
+     c.exchange = QByteArray("SMART");
+     c.currency = QByteArray("USD");
+
+
+      BracketOrder(ib.getOrderId(),parent , takeProfit, stopLoss, parent.action, quantity, price,tp, sl);
+      ib.placeOrder(parent.orderId, c, parent);
+      ib.placeOrder(takeProfit.orderId, c, takeProfit);
+      ib.placeOrder(stopLoss.orderId, c, stopLoss);
+
+}
+
+void TestIbQt::BracketOrder(int parentOrderId, Order &parent, Order &takeProfit, Order &stopLoss, QByteArray action, double quantity, double limitPrice, double takeProfitLimitPrice, double stopLossPrice)
+{
+    //This will be our main or "parent" order
+     parent.orderId = parentOrderId;
+     parent.action = action;
+     parent.orderType = "LMT";
+     parent.totalQuantity = quantity;
+     parent.lmtPrice = limitPrice;
+     //The parent and children orders will need this attribute set to false to prevent accidental executions.
+     //The LAST CHILD will have it set to true,
+     parent.transmit = false;
+     takeProfit.orderId = parent.orderId + 1;
+     takeProfit.action = (action == "BUY") ? "SELL" : "BUY";
+     takeProfit.orderType = "LMT";
+     takeProfit.totalQuantity = quantity;
+     takeProfit.lmtPrice = takeProfitLimitPrice;
+     takeProfit.parentId = parentOrderId;
+     takeProfit.transmit = false;
+     stopLoss.orderId = parent.orderId + 2;
+     stopLoss.action = (action == "BUY") ? "SELL" : "BUY";
+     stopLoss.orderType = "STP";
+     //Stop trigger price
+     stopLoss.auxPrice = stopLossPrice;
+     stopLoss.totalQuantity = quantity;
+     stopLoss.parentId = parentOrderId;
+     //In this case, the low side order will be the last child being sent. Therefore, it needs to set this attribute to true
+     //to activate all its predecessors
+     stopLoss.transmit = true;
 }
 
 void TestIbQt::run()
--- test/testibqt.h
+++ test/testibqt.h
@@ -12,10 +12,11 @@ public:
 
 
     //orders
-    void marketOrder(char dir,QByteArray stockName,int quantity);
+    void marketOrder(char dir, QByteArray stockName, QByteArray secType, int quantity);
+    void limitOrder(char dir, QByteArray stockName, QByteArray secType, int quantity, double price);
+    void limitOrder(char dir, QByteArray stockName, QByteArray secType, int quantity, double price, double sl,double tp);
 
-
-
+    void BracketOrder(int parentOrderId, Order& parent, Order& takeProfit, Order& stopLoss, QByteArray action, double quantity, double limitPrice, double takeProfitLimitPrice, double stopLossPrice);
 
 
 public slots:
